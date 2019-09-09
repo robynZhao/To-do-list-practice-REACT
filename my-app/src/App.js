7d684de9d04104f317797181347f94e11965d696
robynZhao <zhaorobyn895@gmail.com>
2019-09-08 20:34:02 -0500

    introduce state into App class

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,20 @@ import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
+    // introduce state into App class
+    // giving state an empty array to hold all to-dos,
+    // giving an empty string to hold an individual to-do.
+    constructor(props) {
+        super(props);
+        this.state={
+            todos: [{
+                //task: '',
+                //id: '',
+                //completed: false
+            }],
+            todo: ''
+        }
+    }
   render() {
       return (
           <div className="App">Toto list practice</div>);
